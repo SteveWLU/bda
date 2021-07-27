fc6fa53e7b0ef2df6f9bc6a152071a2d461e297d
Stephen P. McCormickgit status <mccormicks@wlu.edu>
2021-07-27 18:55:12 +0200

    SPM UI dev

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from 'react-router-dom';
-import error404 from './error404';
-import HomePage from './HomePage';
-import Navigation from './components/Navigation';
-// import Guerrino from './pages/guerrino'
 
-function App () {
+function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navigation />
-        <div>
-          <Switch>
-            <Route path="/" component={HomePage} exact />
-            <Route path="*" component={error404} />
-            {/* <Route path="/guerrino" component={Guerrino} /> */}
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
